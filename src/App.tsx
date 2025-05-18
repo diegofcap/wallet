56b8883581db5734a5824d5dafc973e19f52b22c
Diego Ferreira <diego@coyote.inf.br>
2025-05-18 11:34:47 -0300

    config: add fonts to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
+
+import {
+	useFonts,
+	Poppins_300Light,
+	Poppins_400Regular,
+	Poppins_500Medium,
+	Poppins_700Bold,
+	Poppins_800ExtraBold,
+} from "@expo-google-fonts/poppins";
+
+import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 
 const App = () => {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: 20 }}>Wallet App</Text>
-        </View>
-    );
+	const [fontsLoaded] = useFonts({
+		Poppins_300Light,
+		Poppins_400Regular,
+		Poppins_500Medium,
+		Poppins_700Bold,
+		Poppins_800ExtraBold,
+		DMSans_400Regular,
+		DMSerifDisplay_400Regular,
+	});
+
+	if (!fontsLoaded) {
+		return <ActivityIndicator />;
+	}
+
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text style={{ fontSize: 20 }}>Wallet App</Text>
+		</View>
+	);
 }
 
 export { App };
