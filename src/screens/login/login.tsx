67de151855693ac719abb9e0792c55562be8b0b3
Diego Ferreira <diego@coyote.inf.br>
2025-05-19 00:21:12 -0300

    feat: add buttons to login page

--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,6 +1,9 @@
 import type React from 'react';
 import { SafeAreaView } from 'react-native';
 import { Container, ContentBody, ContentFooter, ContentHeader, Description, Title, ViewButton } from './styles';
+import { ButtonSocialGoogle } from '@src/components/ButtonSocialGoogle/ButtonSocialGoogle';
+import { ButtonSocial } from '@src/components/ButtonSocial/ButtonSocial';
+
 
 const Login: React.FC = () => {
 	return (
@@ -9,7 +12,10 @@ const Login: React.FC = () => {
 				<ContentHeader>
 					<Title> Seja bem vindo(a) {"\n"} a App Carteira</Title>
 					<Description>Entrar com rede sociais</Description>
-					<ViewButton></ViewButton>
+					<ViewButton>
+						<ButtonSocialGoogle title="Google" />
+						<ButtonSocial iconName="facebook" title="Facebook" />
+					</ViewButton>
 				</ContentHeader>
 				<ContentBody></ContentBody>
 				<ContentFooter></ContentFooter>
