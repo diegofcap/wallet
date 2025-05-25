441284c8a4213a67d5c61d7d6ddf87098f16cd72
Diego Ferreira <diego@coyote.inf.br>
2025-05-24 21:47:58 -0300

    feat: add Notification page

--- src/screens/Tab/Notificacao/Notificacao.tsx
+++ src/screens/Tab/Notificacao/Notificacao.tsx
@@ -1,12 +1,95 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from 'react';
+import { FlatList } from 'react-native';
+import { useTheme } from 'styled-components/native';
+import { ArrowCircleUp } from 'phosphor-react-native';
+
+import { Header } from '@src/components/Header';
+import { new_notifications } from '@src/utils/new_notifications';
+import { ContentTop } from '@src/components/Notifications/ContentTop';
+import { recents_notifications } from '@src/utils/recent_notifications';
+
+import {
+	Container,
+	ContentBody,
+	ContentFlat,
+	ContentHeader,
+	ContentTopTitle,
+	ContentBodyTitle,
+	DataNotifications,
+	TitleNotifications,
+	NewNotificationsFlat,
+	DataNotificationsText,
+	TitleNotificationsText,
+	NewButtonNotifications,
+	DescriptionNotifications,
+	DescriptionNotificationsText,
+} from './styles';
 
 export const Notificacao = () => {
+	const { COLORS } = useTheme();
+
 	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }
-		}>
-			<Text style={{ fontSize: 24, fontWeight: 'bold' }}> Notificação Screen </Text>
-			< Text style={{ fontSize: 16, color: 'gray' }}> This is the notificação screen.</Text>
-		</View>
+		<>
+			<Header
+				iconLeft
+				typeNotification
+				appName='Notificação'
+			/>
+			<Container>
+				<ContentHeader>
+					<ContentTopTitle>Novo</ContentTopTitle>
+					<FlatList
+						data={new_notifications}
+						renderItem={({ item }) => (
+							<ContentTop
+								datetime={item.datetime}
+								title={item.title}
+								type={item.type}
+							/>
+						)}
+						showsVerticalScrollIndicator={false}
+					/>
+				</ContentHeader>
+
+				<ContentBody>
+					<ContentBodyTitle>Recentes</ContentBodyTitle>
+					<FlatList
+						data={recents_notifications}
+						keyExtractor={(item) => item.id.toString()}
+						renderItem={({ item }) => (
+							<ContentFlat>
+								<NewNotificationsFlat>
+									<DataNotifications>
+										<DataNotificationsText>
+											{item.datetime}
+										</DataNotificationsText>
+									</DataNotifications>
+
+									<TitleNotifications>
+										<TitleNotificationsText>
+											{item.title}
+										</TitleNotificationsText>
+									</TitleNotifications>
+
+									<DescriptionNotifications>
+										<DescriptionNotificationsText>
+											{item.type}
+										</DescriptionNotificationsText>
+									</DescriptionNotifications>
+								</NewNotificationsFlat>
+
+								<NewButtonNotifications>
+									<ArrowCircleUp size={32} color={COLORS.GREEN2} />
+								</NewButtonNotifications>
+							</ContentFlat>
+						)}
+						contentContainerStyle={{
+							paddingBottom: 20
+						}}
+						showsVerticalScrollIndicator={false}
+					/>
+				</ContentBody>
+			</Container>
+		</>
 	)
 }
