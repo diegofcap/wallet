f683566c82c57f91c0a647e86e37b2b8e32efcbe
Diego Ferreira <diego@coyote.inf.br>
2025-05-24 21:10:33 -0300

    feat: add profile page

--- src/screens/App/Profile/Profile.tsx
+++ src/screens/App/Profile/Profile.tsx
@@ -1,12 +1,81 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { useTheme } from 'styled-components/native';
+import { Pen, Trash } from 'phosphor-react-native';
+
+import { Profile as ComponentProfile } from '../../../components/Profile';
+import { GoBack } from '../../../components/GoBack';
+import {
+	Container,
+	Header,
+	Content,
+	Footer,
+	Avatar,
+	ViewHeader,
+	HeaderName,
+	IconButton,
+	ViewFooter,
+	TitleFooter,
+	ViewIconButton,
+} from './styles';
 
 export const Profile = () => {
+	const { COLORS } = useTheme();
+
 	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }
-		}>
-			<Text style={{ fontSize: 24, fontWeight: 'bold' }}> Settings Screen </Text>
-			< Text style={{ fontSize: 16, color: 'gray' }}> This is the settings screen.</Text>
-		</View>
+		<>
+			<GoBack />
+			<Container>
+				<Header>
+					<Avatar
+						source={{
+							uri: 'https://avatars.githubusercontent.com/u/14061926?v=4'
+						}}
+					/>
+					<ViewHeader>
+						<HeaderName>Diego Ferreira</HeaderName>
+						<IconButton>
+							<Pen
+								size={25}
+								weight="light"
+								color={COLORS.PURPLE_CARD}
+							/>
+						</IconButton>
+					</ViewHeader>
+				</Header>
+				<Content>
+					<ComponentProfile
+						name='Conta Conectada'
+						onPress={() => { }}
+					/>
+					<ComponentProfile
+						name='Privacidade e Segurança'
+						onPress={() => { }}
+					/>
+					<ComponentProfile
+						name='Configuração de Login'
+						onPress={() => { }}
+					/>
+					<ComponentProfile
+						name='Centro de Serviço'
+						onPress={() => { }}
+					/>
+				</Content>
+				<Footer>
+					<ViewFooter>
+						<TouchableOpacity>
+							<ViewIconButton>
+								<Trash
+									size={36}
+									weight="light"
+									color={COLORS.PURPLE_CARD}
+								/>
+							</ViewIconButton>
+						</TouchableOpacity>
+						<TitleFooter>Deletar Conta</TitleFooter>
+					</ViewFooter>
+				</Footer>
+			</Container>
+		</>
 	)
 }
